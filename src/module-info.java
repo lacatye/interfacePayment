043383ba164abcfe688d7579f815fb4643fe04b4
Thalyta Diniz <thalytadiniz03@gmail.com>
2024-01-23 16:35:34 -0300

    POO Made

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module contract_processing {
+}
---
+++ src/services/PaypalService.java
@@ -0,0 +1,17 @@
+package services;
+
+public class PaypalService implements OnlinePaymentService {
+
+	@Override
+	public double paymentFee(Double amount) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double interest(Double amount, Integer months) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
